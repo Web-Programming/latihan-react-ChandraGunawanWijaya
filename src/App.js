c8bee5e2a03a461e7e4c95b9edba8b2581807621
ChandraGunawanWijaya <161033234+ChandraGunawanWijaya@users.noreply.github.com>
2024-10-03 20:30:12 +0700

    chan

--- src/App.js
+++ src/App.js
@@ -1,77 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Hello from './Hello';
-import Product from './product';
-import Registrasi from './Registrasi';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import routes from './route';
+
 
 function App() {
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
-      <div className='App'>
-        <NavLink to="/" className="navbar-brand" exact="true">
-        Navbar
-        </NavLink>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <NavLink to="" className="navbar-brand" exact="true">
+              Navbar
+            </NavLink>
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+          <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li className="nav-item">
+          <NavLink to="/product" className="nav-link">
+            Product
+          </NavLink>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li className="nav-item">
+          <NavLink to="/register" className="nav-link">
+            Registrasi
+          </NavLink>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
-  </div>
-</nav>
+    </nav>
 
-    <div className='Main'>
+    <div className="main">
       <Routes>
         {
-          routes.map((route,i)) => {
-            const {path, Component} = route;
-            return <Route key={i} path = {path} element={<Component />} />;
-          ))
-          }
-        }
+          routes.map((route, i) => {
+            const { path, Component } = route;
+            return (
+              <Route key={i} path={path} element={<Component />} />
+            );
+          })}
       </Routes>
     </div>
-      </div>
-    </React.Suspense>
+  </div>
+  </React.Suspense>
   );
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <Hello nama="Chandra Gunawan" pesan="Selamat datang di kelas IF31 PW2"></Hello>
-  //       <Hello></Hello>
-  //       <Product></Product>
-  //       <Registrasi></Registrasi>
-
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
--- src/route.js
+++ src/route.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Home = React.lazy(() => import("./Hello"));
 const product = React.lazy(() => import("./Product"));
-const Register = React.lazy(() => import("./Register"));
+const Register = React.lazy(() => import("./Registrasi"));
 
 const routes = [
     {path : "/" , Component : Home},
-    {path : "/product" , Component : Product},
-    {path : "/register" , Component : Register},
+    {path : "/Product" , Component : product},
+    {path : "/Registrasi" , Component : Register},
 ];
 
 export default routes;
